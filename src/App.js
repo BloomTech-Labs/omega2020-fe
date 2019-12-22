db364116ff1e9e690e19bfd046d4834e56423091
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-12-22 15:23:33 +0600

    Added Navigation and dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import NavBar from "./components/Navigation";
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Omega2020 App</h1>
+      <h1>Omega2020 App</h1> 
     </div>
   );
 }
+
+
+
+
 
 export default App;
