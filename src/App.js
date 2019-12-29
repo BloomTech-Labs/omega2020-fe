21447e623be7a8c5a4e709813f8f2938c07cd77d
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-12-29 18:40:15 +0600

    Added privateRoute, utils, auth_config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.css';
-import NavBar from "./components/Navigation";
+import { Router, Route, Switch } from "react-router-dom";
+import { useAuth0 } from "./react-auth0-spa";
+import NavBar from "./components/NavBar";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
+import SudokuBoard from "./components/SudokuBoard"
 
+const App = () => {
+  const { loading } = useAuth0();
 
-
-function App() {
+  if (loading) {
+    return <div>Loading..</div>;
+  }
   return (
+    <Router history={history}>
     <div className="App">
+      <Switch>
+          {/* <Route path="/" exact component={Home} /> */}
+          <PrivateRoute path="/board" component={SudokuBoard} />
+      </Switch>
+      
       <h1>Omega2020 App</h1> 
     </div>
+    </Router>
   );
 }
-
-
-
-
 
 export default App;
--- src/react-auth0-spa.js
+++ src/react-auth0-spa.js
@@ -88,3 +88,6 @@ export const Auth0Provider = ({
 
 
 
+
+
+
