eb35fa93e2d6372e13380749ba59351e30c9f695
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-01-08 13:28:11 -0800

    formatted puzzle showing with no data yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Soduku from './components/Sudoku.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Omega2020 App</h1>
+      <Soduku />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Sudoku.js
+++ src/components/Sudoku.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import Board from './Board';
 import { solvedPuzzle, unsolvedPuzzle } from './Puzzles';
-import './Sudoku.css';
+// import './Sudoku.css';
 
-export default Sudoku = () => {
+const Sudoku = () => {
+    const getFormattedPuzzle = () => {
+      const puzzle = getRandomPuzzle();
+      const formattedPuzzle = formatPuzzle(puzzle);
+      return formattedPuzzle;
+    }
     const [gameBoardState, setGameBoardState] = useState(
     {
             boardState : getFormattedPuzzle(),
@@ -12,97 +17,96 @@ export default Sudoku = () => {
     });
   
 
-  getRandomPuzzle = () => {
+  function getRandomPuzzle() {
     return unsolvedPuzzle;
   }
 
-  getDeepCopyOfArray = (arr) => {
+  function getDeepCopyOfArray(arr) {
     return JSON.parse(JSON.stringify(arr));
   }
 
-  handleSquareValueChange = (i, j, newValue) => {
-    this.setState(prevState => {
-      const newBoardState = this.getDeepCopyOfArray(prevState.boardState);
-      const prevEditable = prevState.boardState[i][j].editable;
-      newBoardState[i][j] = {
-        cellValue : newValue,
-        cellId    : this.stringify(i, j),
-        editable  : prevEditable
-      };
+//   const handleSquareValueChange = (i, j, newValue) => {
+//     this.setState(prevState => {
+//       const newBoardState = this.getDeepCopyOfArray(prevState.boardState);
+//       const prevEditable = prevState.boardState[i][j].editable;
+//       newBoardState[i][j] = {
+//         cellValue : newValue,
+//         cellId    : this.stringify(i, j),
+//         editable  : prevEditable
+//       };
 
-      // Now push the previous board state on the history stack
-      const newHistory = this.getDeepCopyOfArray(prevState.history);
-      newHistory.push(prevState.boardState);
+//       // Now push the previous board state on the history stack
+//       const newHistory = this.getDeepCopyOfArray(prevState.history);
+//       newHistory.push(prevState.boardState);
 
-      return {boardState: newBoardState, history: newHistory,conflicts : new Set([])};
-    });
-  }
+//       return {boardState: newBoardState, history: newHistory,conflicts : new Set([])};
+//     });
+//   }
 
-  handleUndoClick = () => {
-    this.setState(prevState => {
-      const newHistory = this.getDeepCopyOfArray(prevState.history);
-      const lastBoardState = newHistory.pop();
+//   const handleUndoClick = () => {
+//     this.setState(prevState => {
+//       const newHistory = this.getDeepCopyOfArray(prevState.history);
+//       const lastBoardState = newHistory.pop();
 
-      // Now assign the previous board state as the current board state
-      return {boardState:lastBoardState, history:newHistory,conflicts : new Set([])};
-    });
-  }
+//       // Now assign the previous board state as the current board state
+//       return {boardState:lastBoardState, history:newHistory,conflicts : new Set([])};
+//     });
+//   }
 
-  handleNewGameClick = () => {
-    this.setState({
-      boardState: this.getFormattedPuzzle(),
-      history   : [],
-      conflicts : new Set([])
-    });
-  }
+//   const handleNewGameClick = () => {
+//     this.setState({
+//       boardState: this.getFormattedPuzzle(),
+//       history   : [],
+//       conflicts : new Set([])
+//     });
+//   }
 
-  handleVerifyClick = () => {
-    const boardState = this.state.boardState;
+//   const handleVerifyClick = () => {
+//     const boardState = this.state.boardState;
 
-    // rows[0]/cols[0] -> first row/column
-    const rows = {};
-    const cols = {};
-    // Example: boxes['00'] -> an array of values in the first box. 
-    const boxes = {};
+//     // rows[0]/cols[0] -> first row/column
+//     const rows = {};
+//     const cols = {};
+//     // Example: boxes['00'] -> an array of values in the first box. 
+//     const boxes = {};
 
-    for(let i=0; i<boardState.length; i++) {
-      rows[i] = this.getDeepCopyOfArray(boardState[i]);
-      for(let j=0; j<boardState[i].length;j++) {
-        // populating cols
-        if(cols.hasOwnProperty(j)) {
-          cols[j].push(boardState[i][j]);
-        } else {
-          cols[j] = [boardState[i][j]];
-        }
+//     for(let i=0; i<boardState.length; i++) {
+//       rows[i] = this.getDeepCopyOfArray(boardState[i]);
+//       for(let j=0; j<boardState[i].length;j++) {
+//         // populating cols
+//         if(cols.hasOwnProperty(j)) {
+//           cols[j].push(boardState[i][j]);
+//         } else {
+//           cols[j] = [boardState[i][j]];
+//         }
 
-        // populating boxes
-        const boxId = this.stringify(Math.floor(i/3), Math.floor(j/3));
-        if(boxes.hasOwnProperty(boxId)) {
-          boxes[boxId].push(boardState[i][j]);
-        } else {
-          boxes[boxId] = [boardState[i][j]];
-        }
-      }
-    }
+//         // populating boxes
+//         const boxId = this.stringify(Math.floor(i/3), Math.floor(j/3));
+//         if(boxes.hasOwnProperty(boxId)) {
+//           boxes[boxId].push(boardState[i][j]);
+//         } else {
+//           boxes[boxId] = [boardState[i][j]];
+//         }
+//       }
+//     }
 
-    const rowConflicts = this.flatten(this.getConflicts(Object.values(rows)));
-    const colConflicts = this.flatten(this.getConflicts(Object.values(cols)));
-    const boxConflicts = this.flatten(this.getConflicts(Object.values(boxes)));
+//     const rowConflicts = this.flatten(this.getConflicts(Object.values(rows)));
+//     const colConflicts = this.flatten(this.getConflicts(Object.values(cols)));
+//     const boxConflicts = this.flatten(this.getConflicts(Object.values(boxes)));
 
-    const mergedConflicts = [...rowConflicts, ...colConflicts, ...boxConflicts];
-    this.setState({conflicts: new Set(mergedConflicts)});
-  }
+//     const mergedConflicts = [...rowConflicts, ...colConflicts, ...boxConflicts];
+//     this.setState({conflicts: new Set(mergedConflicts)});
+//   }
 
-  flatten = (a) => {
-    return Array.isArray(a) ? [].concat(...a.map(this.flatten)) : a;
+  function flatten(a) {
+    return Array.isArray(a) ? [].concat(...a.map(flatten)) : a;
 }
 
-  getConflicts = (arrs) => {
-    return (arrs
-            .map(arr => this.getConflictsInArray(arr)));
+  function getConflicts(arrs) {
+    return (arrs.map(arr => getConflictsInArray(arr)));
   }
 
-  getConflictsInArray = (arr) => {
+  function getConflictsInArray(arr) {
     const conflictMap = {};
     for(let i=0; i<arr.length; i++) {
       let curr = arr[i];
@@ -117,32 +121,33 @@ export default Sudoku = () => {
     return Object.values(conflictMap).filter(arr => arr.length>1); 
   }
 
-  formatPuzzle = (puzzle) => {
+  function formatPuzzle(puzzle) {
     const formattedPuzzle = createArray(9, 9);
     for(let i=0; i<puzzle.length; i++) {
-      const rowId = this.getRowId(i);
-      const colId = this.getColId(i);
+      const rowId = getRowId(i);
+      const colId = getColId(i);
 
-      const editable = puzzle[i] === '0' ? true : false;
+      const editable = puzzle[i] === '.' ? true : false;
 
       formattedPuzzle[rowId][colId] = {
-                                        cellValue : puzzle[i],
-                                        cellId    : this.stringify(rowId, colId),
-                                        editable  : editable
-                                      };
+                            cellValue : puzzle[i],
+                            cellId    : stringify(rowId, colId),
+                            editable  : editable
+                        };
     }
+    console.log("Formatted Puzzle", formattedPuzzle);
     return formattedPuzzle;
   }
 
-  stringify = (num1, num2) => {
+  function stringify(num1, num2) {
     return num1 + '' + num2;
   }
 
-  getRowId = (i) => {
+  function getRowId(i) {
     return Math.floor(i/9);
   }
 
-  getColId = (i) => {
+  function getColId(i) {
     return (i%9);
   }
 
@@ -159,28 +164,22 @@ export default Sudoku = () => {
       editable : true if this cell will be user defined, false otherwise
     }
   */  
-  getFormattedPuzzle = () => {
-    const puzzle = getRandomPuzzle();
-    const formattedPuzzle = formatPuzzle(puzzle);
-    return formattedPuzzle;
-  }
 
-  render() {
     return (
       <div className = "Sudoku">
         <h1 className="sudokuHeader">Sudoku!</h1>
-        <Board
-          boardState          = {this.state.boardState}
-          conflicts           = {this.state.conflicts}
-          onNewGameClick      = {this.handleNewGameClick}
-          onSquareValueChange = {this.handleSquareValueChange}
-          historyLength       = {this.state.history.length}
-          onUndoClick         = {this.handleUndoClick}
-          onVerifyClick       = {this.handleVerifyClick}
-        />
+        {/* <Board
+          boardState          = {gameBoardState}
+        //   conflicts           = {state.conflicts}
+        //   onNewGameClick      = {handleNewGameClick}
+        //   onSquareValueChange = {handleSquareValueChange}
+        //   historyLength       = {state.history.length}
+        //   onUndoClick         = {handleUndoClick}
+        //   onVerifyClick       = {handleVerifyClick}
+        /> */}
       </div>
     );
-  }
+  
 }
 
 function createArray(length) {
@@ -194,3 +193,5 @@ function createArray(length) {
 
     return arr;
 }
+
+export default Sudoku;
