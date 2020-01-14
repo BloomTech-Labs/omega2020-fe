eecbc0a8c216722c5d72112126bd92eeffc14114
Connor Sullivan <crsullivan@protonmail.com>
2020-01-13 23:58:37 -0700

    adds styling to landing

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
     <NavBar />
       <Switch>
-        <Route path="/home" component={Landing} />
+        <Route exact path="/home" component={Landing} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Registration} />
           {/* <Route path="/" exact component={Home} /> */}
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,143 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Omega2020
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  buttonGrid: {
+    textAlign: 'center',
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [
+    {
+        id:1,
+        title:'Learn',
+        description:'Read our tutorials and learn the best practices for solving almost any sudoku puzzle (coming soon).'
+    },
+    {
+        id:2,
+        title:'Play',
+        description:'Create an account and enjoy different play modes. Guest play is also welcomed! (coming soon).'
+    },
+    {
+        id:3,
+        title:'Solve',
+        description:'Complete every puzzle you begin with our "Solve Square" option. Everyone is a winner! (coming soon).'
+    }
+];
 
 const Landing = () => {
-    return (
-        <Button variant="contained" color="primary">
-            Hello World
-        </Button>
-    )
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <main>
+        {/* Hero unit */}
+          <Container className={classes.image} maxWidth="md">
+          <img src="src\images\omega2020.PNG"  width="400" height="400" alt="Omega2020 logo" />
+        
+            </Container>
+            <Container className={classes.buttonGrid} maxWidth='md'>
+
+                <Button href="http://localhost:3000/register">Create Account</Button>
+                <Button href="http://localhost:3000/puzzle">Guest Login</Button>
+                <Button href="http://localhost:3000/login">Sign in</Button>
+            </Container>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.title}
+                    </Typography>
+                    <Typography>
+                      {card.description}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
 }
 
 export default Landing
