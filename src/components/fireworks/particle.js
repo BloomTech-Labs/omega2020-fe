9957bca510a3e3d0b9875a382d283c0489b87d5e
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-01-21 08:12:53 -0800

    unadded dependency forced code comment-out in particle.js

--- src/components/fireworks/particle.js
+++ src/components/fireworks/particle.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 function Particle(x,y) {
-    this.pos = createVector(x,y);
-    this.vel = createVector(0,-10);
-    this.acc = createVector(0,0);
+    // this.pos = createVector(x,y);
+    // this.vel = createVector(0,-10);
+    // this.acc = createVector(0,0);
 
-    this.applyForce = function(force) {
-        this.acc.add(force);
-    }
-    this.update = function() {
-        this.vel.add(this.acc);
-        this.pas.add(this.vel);
-        this.acc.mult(0);
-    }
+    // this.applyForce = function(force) {
+    //     this.acc.add(force);
+    // }
+    // this.update = function() {
+    //     this.vel.add(this.acc);
+    //     this.pas.add(this.vel);
+    //     this.acc.mult(0);
+    // }
 
-    this.show = function() {
-        point(this.pos.x, this.pos.y)
-    }
+    // this.show = function() {
+    //     point(this.pos.x, this.pos.y)
+    // }
 }
 export default Particle;
--- src/utils/validateWin.js
+++ src/utils/validateWin.js
@@ -1,13 +1,13 @@
 // If board has all squares filled and no conflicts, compare board state string with the solved puzzle string.
 // if board state string = solved puzzle string, then win animation
 
-import React from 'react';
-import { Setup, Draw } from '../components/fireworks/sketch.js';
-import Particle from '../components/fireworks/particle.js';
+// import React from 'react';
+// import { Setup, Draw } from '../components/fireworks/sketch.js';
+// import Particle from '../components/fireworks/particle.js';
 
 const validateWin = ({ boardState, solvedPuzzle, conflicts }) => {
     if (boardState === solvedPuzzle){
-        if (conflicts.length = 0){
+        if (conflicts.length === 0){
         return (
             // build some animation for win here
             alert('Congratulations! You have solved the puzzle!')
