92f55c5e79cd637333f32323d66a150c677a4c79
Connor Sullivan <crsullivan@protonmail.com>
2020-01-28 18:13:54 -0700

    merged

--- src/components/save-resume/getSavedPuzzle.js
+++ src/components/save-resume/getSavedPuzzle.js
@@ -1,17 +1,6 @@
 import axiosWithAuth from '../../utils/axiosWithAuth';
 
 function GetSavedPuzzle() {
-<<<<<<< HEAD
- 
-    return axiosWithAuth()
-        .get("/user-puzzles")
-        .then(res => {
-        console.log("AXIOS from GetSavedPuzzle", res.data);
-        return res.data;
-        })
-        .catch(err => console.log(err.response));
-};
-=======
     return axiosWithAuth()
             .get('/user-puzzles')
             .then(res => {
@@ -21,5 +10,4 @@ function GetSavedPuzzle() {
             .catch(err => console.log(err.response));
 };
 
->>>>>>> 94ae14b811ec09e44a4145976565ac3347d39ea8
 export default GetSavedPuzzle;
