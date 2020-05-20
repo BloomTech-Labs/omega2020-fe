80d2b7aa509d32b7320bb017881428d64dd8ce8c
Jessica <Jessica@users-iMac.local>
2020-05-20 10:32:42 -0400

    configures coverage reporter

--- src/tests/Sudoku.test.js
+++ src/tests/Sudoku.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import App from '../../App.js'
-import Sudoku from '../puzzles-by-difficulty/random/Sudoku';
-import SudokuButtons from '../SudokuButtons';
+import App from '../App.js'
+import Sudoku from '../components/puzzles-by-difficulty/random/Sudoku';
+import SudokuButtons from '../components/SudokuButtons';
 
 
 test('Landing page renders Omega2020', () => {
