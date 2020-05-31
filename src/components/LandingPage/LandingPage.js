feafc7fdd5513f721e15949fccf6d5b4bfd9dd64
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-05-30 20:37:57 -0400

    File debugging so that it can pass in netlify

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
 
-const LandingPage = (props) => {};
+const LandingPage = () => {
+  return (
+    <React.Fragment>
+      <Banner />
+      <Info />
+    </React.Fragment>
+  );
+};
 
 export default LandingPage;
