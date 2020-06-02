e7075de48c7fffa315029ac2010a30633564e639
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-01 23:06:26 -0400

    .env

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 
 // Updated
+
 import Login from './components/auth/Login';
 import Registration from './components/auth/Register';
 import LandingPage from './components/LandingPage/LandingPage';
