3b77b1e8134e828fa8c4aab65a91c114136f243e
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-04 13:40:01 -0400

    reorganize App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import Sudoku from './components/puzzles-by-difficulty/random/Sudoku';
-import NavCondition from './components/Navs/NavCondition';
+
+// General utilities
 import history from './utils/history';
+import './App.css';
+
+// What we've updated
 import Registration from './components/auth/Register';
 import Login from './components/auth/Login';
-import Landing from './components/Landing';
+import LandingPage from './components/LandingPage/LandingPage';
+import SoonPage from './components/soonPage/soon';
 import Footer from './components/Navs/Footer';
+
+// What is left to update
+import Sudoku from './components/puzzles-by-difficulty/random/Sudoku';
+import NavCondition from './components/Navs/NavCondition';
 import ResumedPuzzle from './components/save-resume/ResumedPuzzle';
 import MediumSudoku from './components/puzzles-by-difficulty/medium/MediumSudoku';
 import HardSudoku from './components/puzzles-by-difficulty/hard/HardSudoku';
@@ -15,10 +23,6 @@ import EasySudoku from './components/puzzles-by-difficulty/easy/EasySudoku';
 import UploadForm from './components/upload-image/UploadForm';
 import UploadSudoku2 from './components/upload-image/UploadSudoku2';
 import TeamList from './components/about-team/TeamList';
-import SoonPage from './components/soonPage/soon';
-import './App.css';
-
-import LandingPage from './components/LandingPage/LandingPage';
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token'));
@@ -33,7 +37,6 @@ const App = () => {
         <NavCondition token={token} />
         <Switch>
           <Route exact path='/' component={LandingPage} />
-          {/* <Route path="/login" component={Login} /> */}
           <Route
             path='/login'
             render={(props) => (
@@ -41,7 +44,6 @@ const App = () => {
             )}
           />
 
-          {/* <Route path="/register" component={Registration} /> */}
           <Route
             path='/register'
             render={(props) => (
@@ -54,7 +56,6 @@ const App = () => {
           <Route path='/easy' component={EasySudoku} />
           <Route path='/hard' component={HardSudoku} />
           <Route path='/user-puzzles' component={ResumedPuzzle} />
-          {/* <Route path="/upload" component={UploadImage} /> */}
           <Route path='/upload' component={UploadForm} />
           <Route path='/upload-save' component={UploadSudoku2} />
           <Route path='/about' component={TeamList} />
