ba1bee2822f5bb6a8dd245dd0a65079cb5accac0
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-04 13:24:15 -0400

    connect banner and info component to LandingPage.js

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Banner from './Banner';
+import Info from './Info';
+
+const LandingPage = () => {
+  return (
+    <Box>
+      <Banner />
+      <Info />
+    </Box>
+  );
+};
+
+export default LandingPage;
