573db9d409e65629a9fbac7d5359ee9283c810f9
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-04 14:41:26 -0400

    Update Banner.js responsivness

--- src/components/LandingPage/Banner.js
+++ src/components/LandingPage/Banner.js
@@ -15,86 +15,89 @@ const Banner = (props) => {
 
   return (
     <Box className={classes.root}>
-      <Box className={classes.leftContainer}>
-        <CardHeader
-          avatar={
-            loading ? (
-              <Skeleton
-                className='bannerIMG'
-                variant='rect'
-                width={600}
-                height={600}
-              />
-            ) : (
-              <Box>
-                <img src={banner} alt='banner' />
-              </Box>
-            )
-          }
-        />
-      </Box>
-      <Box className={classes.rightContainer}>
-        <Box className={classes.container}>
+      <Box className={classes.mainContainer}>
+        <Box className={classes.leftContainer}>
           <CardHeader
-            title={
+            avatar={
               loading ? (
                 <Skeleton
-                  className={classes.title}
+                  className='bannerIMG'
+                  variant='rect'
                   width={600}
-                  height={100}
-                  m={0}
-                  p={0}
+                  height={600}
                 />
               ) : (
-                <Typography variant='h4'>
-                  OMEGA 2020 is an online sudoku app. <br /> Learn how to play,
-                  upload your own sudoku games, and compete with friends :&#41;
-                </Typography>
+                <Box className={classes.container}>
+                  {/* <img src={banner} alt='banner' className={classes.img} /> */}
+                </Box>
               )
             }
           />
-          <CardHeader
-            title={
-              loading ? (
-                <Skeleton
-                  className={classes.title}
-                  width={600}
-                  height={100}
-                  m={0}
-                  p={0}
-                />
-              ) : (
-                <img src={puzzle} alt='game' />
-              )
-            }
-          />
-          <CardHeader
-            subheader={
-              loading ? (
-                <Skeleton width={500} height={400} margin={0} padding={0} />
-              ) : (
-                <Typography variant='h6'>HAVE FUN ON THE GO!</Typography>
-              )
-            }
-          />
-          <CardHeader
-            subheader={
-              loading ? (
-                <Skeleton className={classes.text_4} width={60} height={55} />
-              ) : (
-                <ColorButton variant='contained' color='primary'>
-                  Get Started
-                </ColorButton>
-              )
-            }
-          />
+        </Box>
+        <Box className={classes.rightContainer}>
+          <Box className={classes.container}>
+            <CardHeader
+              title={
+                loading ? (
+                  <Skeleton
+                    className={classes.title}
+                    width={600}
+                    height={100}
+                    m={0}
+                    p={0}
+                  />
+                ) : (
+                  <Typography variant='h4'>
+                    OMEGA 2020 is an online sudoku app. <br /> Learn how to
+                    play, upload your own sudoku games, and compete with friends
+                    :&#41;
+                  </Typography>
+                )
+              }
+            />
+            <CardHeader
+              title={
+                loading ? (
+                  <Skeleton
+                    className={classes.title}
+                    width={600}
+                    height={100}
+                    m={0}
+                    p={0}
+                  />
+                ) : (
+                  <img src={puzzle} alt='game' />
+                )
+              }
+            />
+            <CardHeader
+              subheader={
+                loading ? (
+                  <Skeleton width={500} height={400} margin={0} padding={0} />
+                ) : (
+                  <Typography variant='h6'>HAVE FUN ON THE GO!</Typography>
+                )
+              }
+            />
+            <CardHeader
+              subheader={
+                loading ? (
+                  <Skeleton className={classes.text_4} width={60} height={55} />
+                ) : (
+                  <ColorButton variant='contained' color='primary'>
+                    <Typography variant='subtitle2'>Get Started</Typography>
+                  </ColorButton>
+                )
+              }
+            />
+          </Box>
         </Box>
       </Box>
     </Box>
   );
 };
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     flexFlow: 'row wrap',
@@ -103,16 +106,23 @@ const useStyles = makeStyles((theme) => ({
     color: '#FFFFFF',
     padding: 90,
   },
-  leftContainer: {
-    width: '30%',
-    height: 'auto',
+  mainContainer: {
+    width: '100%',
     display: 'flex',
-    flexFlow: 'column wrap',
-    justifyContent: 'center',
+    flexFlow: 'row wrap',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
   },
+  leftContainer: {
+    width: '700px',
+    height: '500px',
+    backgroundImage: `url(${banner})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain, cover',
+    backgroundPosition: 'center',
+  },
   rightContainer: {
-    width: '40%',
+    width: '700px',
     display: 'flex',
     flexFlow: 'column wrap',
     justifyContent: 'center',
@@ -127,6 +137,11 @@ const useStyles = makeStyles((theme) => ({
     flexFlow: 'column wrap',
     justifyContent: 'center',
     border: '2px solid red',
+  },
+  img: {
+    width: '100%',
+    maxWidth: '700px',
+    height: 'auto',
   },
 }));
 
