76460da9955b313833319ed62831ed1715de9cbf
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-05 14:47:10 -0400

    Add and render Teams component

--- src/components/AboutUsPage/AboutUsPage.js
+++ src/components/AboutUsPage/AboutUsPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import AboutUs from './AboutUs';
+import Teams from './Teams';
 
 const AboutUsPage = () => {
   return (
     <Box>
       <AboutUs />
+      <Teams />
     </Box>
   );
 };
