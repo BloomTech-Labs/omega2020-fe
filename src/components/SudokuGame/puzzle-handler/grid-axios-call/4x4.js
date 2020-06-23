fc6b521b983607083faf1da388cffb7b5e48493e
alexis-anderson <aanderson9313@gmail.com>
2020-06-23 13:59:15 -0400

    added axios calls for all grids, refactored puzzlehandler/easysudoku for reusablility

---
+++ src/components/SudokuGame/puzzle-handler/grid-axios-call/4x4.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const Get4x4 = () => axios
+  .get('https://omega2020.herokuapp.com/puzzle/4x4')
+  .then((res) => {
+    console.log('AXIOS', res.data);
+    return res.data;
+  })
+  .catch((err) => console.log(err.response));
