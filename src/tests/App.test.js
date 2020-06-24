b3b83941a3e5b7c9fab4922a8e6f3cfd574f794d
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-24 18:06:46 -0400

    App.js renders content - test passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import * as rtl from 'react-testing-library';
-import 'jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-import App from './App';
+import App from '../App.js';
+
+it('renders content', () => {
+  const wrapper = rtl.render(<App />);
+  console.log(wrapper.debug());
+});
