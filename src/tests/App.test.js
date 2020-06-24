3eae1ac86b1abb82e27109c123842a4398a46be3
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-24 17:53:41 -0400

    add jest-dom

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
-import MemoryRouter from 'react-router-dom';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
-
-import App from '../App';
-
-test('There is Omega2020 in the app', () => {
-    expect('Omega2020').toMatch(/meg/);
-});
-
+import App from './App';
