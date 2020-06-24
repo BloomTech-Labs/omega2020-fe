dacf9d933bd42343b6e62e6a6a6645c39c6c58d9
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-24 18:34:51 -0400

    App.js contains text Omega 2020 test passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import * as rtl from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from '../App.js';
 
-it('renders content', () => {
-  const wrapper = rtl.render(<App />);
-  console.log(wrapper.debug());
+it('App renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+it('App.js contains text Omega 2020', () => {
+  const { getByText } = render(<App />);
+  const element = getByText('Omega 2020');
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
 });
