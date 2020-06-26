c0405aa4a112cbf24e2f3749fe04d9a97312342e
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-06-25 21:59:45 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const App = () => {
             <Route path='/tutorial' component={Tutorial} />
             <Route path='/coming-soon' component={SoonPage} />
 
-            {/* ----------------------------------------------- */}
+            {/* ---------------------------------------------- */}
 
             <Route
               path='/9x9Game'
