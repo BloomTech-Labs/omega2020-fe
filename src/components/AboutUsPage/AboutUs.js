770ebcc7da456ef64eeec583bc28fd21ac4f32f1
alexis-anderson <aanderson9313@gmail.com>
2020-07-20 23:57:05 -0400

    fixed the typos

--- src/components/AboutUsPage/AboutUs.js
+++ src/components/AboutUsPage/AboutUs.js
@@ -56,17 +56,6 @@ const AboutUs = (props) => {
                 loading ? (
                   <Skeleton width={400} height={400} />
                 ) : (
-                  <Typography data-testid='subheader'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </Typography>
-                  
                   <Typography>
                     Sudomega started as a Lambda School Labs project,
                     meaning we were given an idea and told to run with it!
