52fb2dd9f27137998d34c16d8940dde2d70bee16
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-08-16 16:33:33 -0400

    gamepage menu bar

---
+++ src/components/assets/ClearButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const ClearButton = (props) => {
+  return (
+    <ColorButton
+      href={props.href}
+      size='large'
+      className='href'
+      color='inherit'
+    >
+      <Typography variant='caption' className='title'>
+        {props.title}
+      </Typography>
+    </ColorButton>
+  );
+};
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    textTransform: 'none',
+  },
+}))(Button);
+
+export default ClearButton;
